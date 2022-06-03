85ec28baf9773094a189de0328946ffb5188ce26
vincentvicran <vvicran@gmail.com>
2022-06-03 16:18:32 +0545

    style: custom styles implemented from css module for homepage Header

--- src/components/HomepageFeatures/components/hoverMenu/hoverMenu.component.jsx
+++ src/components/HomepageFeatures/components/hoverMenu/hoverMenu.component.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import "./css/hoverMenu.css";
 
-import { TransitionBlock, interpolate, AnimatedBlock } from "react-ui-animate";
+import {
+  TransitionBlock,
+  interpolate,
+  AnimatedBlock,
+  AnimationConfigUtils,
+} from "react-ui-animate";
 
 export const HoverMenu = () => {
   const [open, setOpen] = useState(false);
@@ -10,9 +15,7 @@ export const HoverMenu = () => {
   return (
     <div className="hover-menu-container">
       <div
-        className={
-          bounce ? "hover-menu-body hover-menu-bounce" : "hover-menu-body"
-        }
+        className="hover-menu-area"
         onMouseEnter={() => {
           setOpen(true);
           setBounce(false);
@@ -22,76 +25,109 @@ export const HoverMenu = () => {
           setBounce(true);
         }}
       >
-        <h4 className="hover-menu-name">Explore</h4>
-        <div className="hover-menu-options">
-          <TransitionBlock state={open}>
-            {(animation) => (
-              <AnimatedBlock
-                className="hover-menu-1"
-                style={{
-                  opacity: interpolate(animation.value, [0, 1], [0, 1]),
-                  translateX: interpolate(
-                    animation.value,
-                    [0, 1],
-                    ["0px", "-100px"]
-                  ),
-                }}
-              >
-                1
-              </AnimatedBlock>
-            )}
-          </TransitionBlock>
-          <TransitionBlock state={open} className="hover-menu-options">
-            {(animation) => (
-              <AnimatedBlock
-                className="hover-menu-2"
-                style={{
-                  opacity: interpolate(animation.value, [0, 1], [0, 1]),
-                  translateY: interpolate(
-                    animation.value,
-                    [0, 1],
-                    ["0px", "-100px"]
-                  ),
-                }}
-              >
-                2
-              </AnimatedBlock>
-            )}
-          </TransitionBlock>
-          <TransitionBlock state={open} className="hover-menu-options">
-            {(animation) => (
-              <AnimatedBlock
-                className="hover-menu-3"
-                style={{
-                  opacity: interpolate(animation.value, [0, 1], [0, 1]),
-                  translateX: interpolate(
-                    animation.value,
-                    [0, 1],
-                    ["0px", "100px"]
-                  ),
-                }}
-              >
-                3
-              </AnimatedBlock>
-            )}
-          </TransitionBlock>
-          <TransitionBlock state={open} className="hover-menu-options">
-            {(animation) => (
-              <AnimatedBlock
-                className="hover-menu-4"
-                style={{
-                  opacity: interpolate(animation.value, [0, 1], [0, 1]),
-                  translateY: interpolate(
-                    animation.value,
-                    [0, 1],
-                    ["0px", "100px"]
-                  ),
-                }}
-              >
-                4
-              </AnimatedBlock>
-            )}
-          </TransitionBlock>
+        <div
+          className={
+            bounce ? "hover-menu-body hover-menu-bounce" : "hover-menu-body"
+          }
+        >
+          <h4 className="hover-menu-name">Explore</h4>
+          <div className="hover-menu-options">
+            <TransitionBlock
+              state={open}
+              config={{
+                ...AnimationConfigUtils.WOOBLE,
+                friction: 15,
+              }}
+            >
+              {(animation) => (
+                <AnimatedBlock
+                  className="hover-menu-1"
+                  style={{
+                    opacity: interpolate(animation.value, [0, 1], [0, 1]),
+                    translateX: interpolate(
+                      animation.value,
+                      [0, 1],
+                      ["0px", "-70px"]
+                    ),
+                  }}
+                >
+                  1<div className="hover-menu-bottom-border"></div>
+                </AnimatedBlock>
+              )}
+            </TransitionBlock>
+            <TransitionBlock
+              state={open}
+              className="hover-menu-options"
+              config={{
+                ...AnimationConfigUtils.WOOBLE,
+                friction: 15,
+              }}
+            >
+              {(animation) => (
+                <AnimatedBlock
+                  className="hover-menu-2"
+                  style={{
+                    opacity: interpolate(animation.value, [0, 1], [0, 1]),
+                    translateY: interpolate(
+                      animation.value,
+                      [0, 1],
+                      ["0px", "-79px"]
+                    ),
+                  }}
+                >
+                  2<div className="hover-menu-bottom-border"></div>
+                </AnimatedBlock>
+              )}
+            </TransitionBlock>
+            <TransitionBlock
+              state={open}
+              className="hover-menu-options"
+              config={{
+                ...AnimationConfigUtils.WOOBLE,
+                friction: 15,
+              }}
+            >
+              {(animation) => (
+                <AnimatedBlock
+                  className="hover-menu-3"
+                  style={{
+                    opacity: interpolate(animation.value, [0, 1], [0, 1]),
+                    translateX: interpolate(
+                      animation.value,
+                      [0, 1],
+                      ["0px", "70px"]
+                    ),
+                  }}
+                >
+                  3<div className="hover-menu-bottom-border"></div>
+                </AnimatedBlock>
+              )}
+            </TransitionBlock>
+            <TransitionBlock
+              state={open}
+              className="hover-menu-options"
+              config={{
+                ...AnimationConfigUtils.WOOBLE,
+                friction: 15,
+              }}
+            >
+              {(animation) => (
+                <AnimatedBlock
+                  className="hover-menu-4"
+                  style={{
+                    opacity: interpolate(animation.value, [0, 1], [0, 1]),
+                    translateY: interpolate(
+                      animation.value,
+                      [0, 1],
+                      ["0px", "79px"]
+                    ),
+                  }}
+                >
+                  4<div className="hover-menu-bottom-border"></div>
+                </AnimatedBlock>
+              )}
+            </TransitionBlock>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import clsx from "clsx";
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
@@ -9,23 +8,27 @@ import { HomepageFeatures } from "@site/src/components";
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--md"
-            to="/docs/intro"
-          >
-            Get Started
-          </Link>
-          <Link
-            className="button button--secondary button--md"
-            to="https://github.com/dipeshrai123/react-ui-animate"
-          >
-            Try It
-          </Link>
+    <header className={styles.heroBanner}>
+      <div className={styles.heroOuterContainer}>
+        <div className={styles.heroContainer}>
+          <h1 className={styles.heroTitle}>{siteConfig.title}</h1>
+          <p className={styles.heroTagline}>{siteConfig.tagline}</p>
+          <div className={styles.buttonContainer}>
+            <Link
+              className={styles.buttons}
+              // className="button button--secondary button--md"
+              to="/docs/intro"
+            >
+              Get Started
+            </Link>
+            <Link
+              className={styles.buttons}
+              // className="button button--secondary button--md"
+              to="https://github.com/dipeshrai123/react-ui-animate"
+            >
+              Try It
+            </Link>
+          </div>
         </div>
       </div>
     </header>
