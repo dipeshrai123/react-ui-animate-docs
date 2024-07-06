364b2d1a41eb152887f8e24a2a43b1d44fe5cbbf
Dipesh Rai <raidipesh78@gmail.com>
2024-07-06 21:16:05 +0545

    feat: v3.2.0 docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,42 +1,79 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
-/** @type {import('@docusaurus/types').Config} */
-
 const config = {
   title: 'React UI Animate',
-  tagline: 'Create smooth animations and interactive gestures in React applications effortlessly 💻',
+  tagline:
+    'Create smooth animations and interactive gestures in React applications effortlessly 💻',
   url: 'https://www.react-ui-animate.js.org/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/react-ui-animate.ico',
-  organizationName: 'dipeshrai123', // Usually your GitHub org/user name.
-  projectName: 'react-ui-animate-docs', // Usually your repo name.
+  organizationName: 'dipeshrai123',
+  projectName: 'react-ui-animate-docs',
   trailingSlash: false,
   presets: [
     [
       'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          sidebarPath: './sidebars.js',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: ['./src/css/custom.css'],
         },
-      }),
+      },
     ],
-  ]
+  ],
+  themeConfig: {
+    navbar: {
+      title: 'React UI Animate',
+      logo: {
+        alt: 'React UI Animate Logo',
+        src: 'img/react-ui-animate.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          type: 'docsVersionDropdown',
+          dropdownActiveClassDisabled: true,
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/dipeshrai123/react-ui-animate',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Learn',
+          items: [
+            {
+              label: 'Introduction',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/dipeshrai123/react-ui-animate',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()}, React UI Animate`,
+    },
+  },
 };
 
 module.exports = config;
