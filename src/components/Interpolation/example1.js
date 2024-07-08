a94e386af44b237c22c8cdb8eaeaa7fec230e673
Dipesh Rai <raidipesh78@gmail.com>
2024-07-08 13:10:20 +0545

    fix: added reset button on interpolation examples

--- src/components/Interpolation/example1.js
+++ src/components/Interpolation/example1.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { AnimatedBlock, useAnimatedValue, interpolate } from 'react-ui-animate';
-import { Button, Paper } from '@mui/material';
+import {
+  AnimatedBlock,
+  useAnimatedValue,
+  interpolate,
+  withTiming,
+} from 'react-ui-animate';
+import { Button, ButtonGroup, Paper } from '@mui/material';
 
 const Example = () => {
   const left = useAnimatedValue(0);
 
   return (
-    <Paper variant='outlined' style={{ padding: 20, marginBottom: 20 }}>
+    <Paper variant="outlined" style={{ padding: 20, marginBottom: 20 }}>
       <AnimatedBlock
         style={{
           width: interpolate(left.value, [0, 200], [100, 400]),
@@ -18,13 +23,17 @@ const Example = () => {
         }}
       />
 
-      <Button
-        variant='outlined'
-        onClick={() => (left.value = 200)}
-        style={{ marginTop: 10 }}
-      >
-        Animate
-      </Button>
+      <ButtonGroup style={{ marginTop: 10 }}>
+        <Button variant="outlined" onClick={() => (left.value = 200)}>
+          Animate
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={() => (left.value = withTiming(0, { duration: 0 }))}
+        >
+          Reset
+        </Button>
+      </ButtonGroup>
     </Paper>
   );
 };
--- src/components/Interpolation/example2.js
+++ src/components/Interpolation/example2.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { AnimatedBlock, useAnimatedValue, interpolate } from 'react-ui-animate';
-import { Button, Paper } from '@mui/material';
+import {
+  AnimatedBlock,
+  useAnimatedValue,
+  interpolate,
+  withTiming,
+} from 'react-ui-animate';
+import { Button, ButtonGroup, Paper } from '@mui/material';
 
 const Example = () => {
   const opacity = useAnimatedValue(0);
 
   return (
-    <Paper variant='outlined' style={{ padding: 20, marginBottom: 20 }}>
+    <Paper variant="outlined" style={{ padding: 20, marginBottom: 20 }}>
       <AnimatedBlock
         style={{
           width: 100,
@@ -16,13 +21,17 @@ const Example = () => {
         }}
       />
 
-      <Button
-        variant='outlined'
-        onClick={() => (opacity.value = 1)}
-        style={{ marginTop: 10 }}
-      >
-        Animate
-      </Button>
+      <ButtonGroup style={{ marginTop: 10 }}>
+        <Button variant="outlined" onClick={() => (opacity.value = 1)}>
+          Animate
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={() => (opacity.value = withTiming(0, { duration: 0 }))}
+        >
+          Reset
+        </Button>
+      </ButtonGroup>
     </Paper>
   );
 };
