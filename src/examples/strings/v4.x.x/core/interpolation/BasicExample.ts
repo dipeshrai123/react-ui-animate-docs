13c00fad6608bdba1c8d76a6689a47f8a2127893
bibekshrestha <bibekoffice2080@gmail.com>
2025-07-07 10:32:02 +0545

    feat: added basic example

---
+++ src/examples/strings/v4.x.x/core/interpolation/BasicExample.ts
@@ -0,0 +1,24 @@
+const core_interpolation_BasicExample = `import React from 'react';
+import { useValue, animate, withSpring } from 'react-ui-animate';
+
+import './styles.css';
+
+export default function App() {
+  const [x, setX] = useValue(0);
+
+  return (
+    <div>
+      <animate.div
+        style={{
+          width: 100,
+          height: 100,
+          background: x.to([0, 100], ['red', 'teal']),
+          translateX: x,
+        }}
+        onClick={() => setX(withSpring(100))}
+      />
+    </div>
+  );
+}
+`;
+export default core_interpolation_BasicExample;
