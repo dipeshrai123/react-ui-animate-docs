175b61aea58b1ed0efab3164ad0d2f53f57099bc
Dipesh Rai <raidipesh78@gmail.com>
2025-07-09 16:30:42 +0200

    fix: examples updated

--- src/examples/raw/v5.x.x/gesture/useDrag/BasicSetup.tsx
+++ src/examples/raw/v5.x.x/gesture/useDrag/BasicSetup.tsx
@@ -1,21 +1,15 @@
 import React, { useRef } from 'react';
-import { animate, useDrag, useValue } from 'react-ui-animate';
+import { animate, useDrag, useValue, withSpring } from 'react-ui-animate';
 
 const App = () => {
   const ref = useRef<HTMLDivElement>(null);
   const [translateX, setTranslateX] = useValue(0);
   const [translateY, setTranslateY] = useValue(0);
 
-  useDrag(
-    ref,
-    ({ down, movement }) => {
-      setTranslateX(down ? movement.x : 0);
-      setTranslateY(down ? movement.y : 0);
-    },
-    {
-      threshold: 10,
-    }
-  );
+  useDrag(ref, ({ down, movement }) => {
+    setTranslateX(down ? movement.x : withSpring(0));
+    setTranslateY(down ? movement.y : withSpring(0));
+  });
 
   return (
     <animate.div
